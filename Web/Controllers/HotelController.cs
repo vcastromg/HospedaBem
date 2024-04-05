8d4d82ff0e28302cb2c79982c31219ff061245b1
Lucas Carvalho <lucascarvalhopc@gmail.com>
2024-04-05 11:59:37 -0300

    ajusta HotelController

--- Web/Controllers/HotelController.cs
+++ Web/Controllers/HotelController.cs
@@ -4,26 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 namespace HexagonalArchitecture.Controllers;
 
 [ApiController]
-[Route("")]
 [Route("/api")]
-[Route("/api/teste")]
-public class TestController : ControllerBase
+[Route("/api/hotel")]
+public class HotelController : ControllerBase
 {
     [HttpGet]
-    public string Get()
-    {
-        return "Testando";
-    }
-
-    [HttpGet]
-    [Route("/api/teste2")]
-    public string Get2()
-    {
-        return "testando2";
-    }
-
-    [HttpGet]
-    [Route("/api/hotel")]
     public List<Hotel> listHotels()
     {
         Hotel h1 = new Hotel("Hotel1", "address", 4234.32, 4324.32, null);
