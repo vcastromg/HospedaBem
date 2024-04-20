f9b49ae3903312c406e2e1bd626414b31254571f
Victor Castro <victorcastromg@gmail.com>
2024-04-20 10:49:50 -0300

    DTO de pesquisa de acomodações

---
+++ DTOs/RoomSearchDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace DTOs;
+
+public class RoomSearchDTO
+{
+    public DateTime MininumDate { get; set; }
+    public DateTime MaximumDate { get; set; }
+    public string CityName { get; set; }
+    public double MinimumPrice { get; set; }
+    public double MaximumPrice { get; set; }
+    public short MinimumRate { get; set; }
+}
