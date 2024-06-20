0d2d3e78bc2b8b70767273fd1ca934edb4fc8996
LucasCarvalho01 <lucascarvalhopc@gmail.com>
2024-06-19 23:05:15 -0300

    implementa endpoints e serviços para entidade review

---
+++ Application/Repositories/ReviewRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain;
+
+namespace Application.Repositories;
+
+public interface ReviewRepository: BaseRepository<Review>
+{
+    public ICollection<Review> GetReviewsByUser(string userId);
+    public Review UpdateReview(Review updatedReview);
+}
---
+++ Application/Services/ReviewService.cs
@@ -0,0 +1,11 @@
+﻿using Domain;
+
+namespace Application.Services;
+
+public interface ReviewService
+{
+    public void CreateReview(Review review);
+    public ICollection<Review> GetReviewsByUser(string userId);
+    public Review UpdateReview(Review review);
+    public void DeleteReview(long id);
+}
