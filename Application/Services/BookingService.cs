e99fcd4951d11fc5f599680a5cf1ccf22fd511f1
Victor Castro <victorcastromg@gmail.com>
2024-06-20 21:37:06 -0300

    Services de Booking e User

---
+++ Application/Services/BookingService.cs
@@ -0,0 +1,8 @@
+using Domain;
+
+namespace Application.Services;
+
+public interface BookingService
+{
+    Booking Book(DateTime checkin, DateTime checkout, int roomId);
+}
---
+++ Application/Services/UserService.cs
@@ -0,0 +1,6 @@
+namespace Domain;
+
+public interface UserService
+{
+    Booking[] GetUserBookings();
+}
