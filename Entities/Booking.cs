f6f243aa6d546ef5635fd863c89d061c99e1ad75
Andre <75341645+andrembarreto@users.noreply.github.com>
2024-06-20 22:39:44 -0300

    Torna Comment propriedade anulável em Booking

--- Entities/Booking.cs
+++ Entities/Booking.cs
@@ -6,7 +6,7 @@ public class Booking: BaseEntity
 {
     public DateTime CheckIn { get; set; }
     public DateTime CheckOut { get; set; }
-    public string Comment { get; set; }
+    public string? Comment { get; set; }
 
     public virtual AppUser User { get; set; }
     public virtual Room Room { get; set; }
