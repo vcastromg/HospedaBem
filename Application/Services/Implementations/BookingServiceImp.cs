6cf4028cd7727264a296767208a46b383bed2457
Andre <75341645+andrembarreto@users.noreply.github.com>
2024-06-20 22:39:07 -0300

    Define classe que implementa BookingService

---
+++ Application/Services/Implementations/BookingServiceImp.cs
@@ -0,0 +1,18 @@
+﻿using Application.Repositories;
+using Domain;
+
+namespace Application.Services.Implementations;
+public class BookingServiceImp : BookingService
+{
+    private readonly BookingRepository _bookingRepository;
+
+    public BookingServiceImp(BookingRepository bookingRepository)
+    {
+        _bookingRepository = bookingRepository;
+    }
+
+    public Booking Book(DateTime checkin, DateTime checkout, int roomId)
+    {
+        throw new NotImplementedException();
+    }
+}
