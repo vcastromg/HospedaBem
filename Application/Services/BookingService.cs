9ee49db69b5c4abead0187e40f53df89135530de
Lucas Carvalho <lucascarvalhopc@gmail.com>
2024-06-23 19:56:41 -0300

    ajustes endpoint de registar booking

--- Application/Services/BookingService.cs
+++ Application/Services/BookingService.cs
@@ -1,11 +1,12 @@
 using Domain;
 using Domain.Entities;
+using DTOs;
 
 namespace Application.Services;
 
 public interface BookingService
 {
-    void Book(DateTime checkIn, DateTime checkOut, Room room, AppUser user);
+    void Book(CreateBookingDTO dto);
     void CancelBooking(long bookingId);
     bool CheckRoomAvailabilityWithinPeriod(long roomId, DateTime checkIn, DateTime checkOut);
     Booking FindBookingById(string id);
