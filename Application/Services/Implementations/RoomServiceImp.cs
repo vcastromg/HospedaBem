e6d2729bfaf63d7011a3b09d36da5981f5886692
Lucas Carvalho <lucascarvalhopc@gmail.com>
2024-06-23 17:06:01 -0300

    implementa servico para registar quarto

--- Application/Services/Implementations/RoomServiceImp.cs
+++ Application/Services/Implementations/RoomServiceImp.cs
@@ -7,10 +7,12 @@ namespace Application.Services.Implementations;
 public class RoomServiceImp : RoomService
 {
     private readonly RoomRepository _roomRepository;
+    private readonly HotelService _hotelService;
 
-    public RoomServiceImp(RoomRepository roomRepository)
+    public RoomServiceImp(RoomRepository roomRepository, HotelService hotelService)
     {
         _roomRepository = roomRepository;
+        _hotelService = hotelService;
     }
 
     public IEnumerable<Room> Search(RoomSearchDTO dto)
@@ -37,5 +39,22 @@ public class RoomServiceImp : RoomService
         }
 
         return query.ToList();
+    }
+
+    public Room RegisterRoom(RegisterRoomDTO dto)
+    {
+        //find hotel by id
+        var newRoom = new Room()
+        {
+            Number = dto.Number,
+            GuestsCapacity = dto.GuestsCapacity,
+            IsAvailable = dto.IsAvailable,
+            Price = dto.Price,
+            Hotel = null
+        };
+        
+        _roomRepository.Add(newRoom);
+
+        return newRoom;
     }
 }
--- Application/Services/RoomService.cs
+++ Application/Services/RoomService.cs
@@ -6,4 +6,5 @@ namespace Application.Services;
 public interface RoomService
 {
     IEnumerable<Room> Search(RoomSearchDTO dto);
+    Room RegisterRoom(RegisterRoomDTO dto);
 }
