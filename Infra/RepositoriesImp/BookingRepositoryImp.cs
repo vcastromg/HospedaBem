fa8b73123c6e653e912d66cc6e8d6ae6f41cdf55
LucasCarvalho01 <lucascarvalhopc@gmail.com>
2024-06-24 12:45:27 -0300

    implementa BookingRepository

---
+++ Infra/RepositoriesImp/BookingRepositoryImp.cs
@@ -0,0 +1,21 @@
+﻿using Application.Repositories;
+using Domain;
+
+namespace Infra.Repositories.Implementations;
+
+public class BookingRepositoryImp : BaseRepositoryImp<Booking>, BookingRepository
+{
+    private readonly ApplicationDbContext _applicationDbContext;
+
+    public BookingRepositoryImp(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    public ICollection<Booking> GetBookingsByRoomId(long roomId)
+    {
+        return _applicationDbContext.Bookings
+            .Where(b => b.Room.Id == roomId)
+            .ToList();
+    }
+}
