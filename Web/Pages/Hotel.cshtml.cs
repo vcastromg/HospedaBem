eb514d29a71f6be3ca8fde0c713b9bfd39d18598
ericomcolen <erico.colen@gmail.com>
2024-06-26 16:51:00 -0300

    Query de quartos disponíveis

--- Web/Pages/Hotel.cshtml.cs
+++ Web/Pages/Hotel.cshtml.cs
@@ -9,22 +9,39 @@ namespace HexagonalArchitecture.Pages;
 public class HotelModel : PageModel
 {
     private readonly HotelService _hotelService;
-    public Hotel? Hotel;
-    public string MapUrl;
+    public Hotel? Hotel { get; set; }
+    public string MapUrl { get; set; }
+    public DateTime? CheckIn { get; set; }
+    public DateTime? CheckOut { get; set; }
 
     public HotelModel(HotelService hotelService)
     {
         _hotelService = hotelService;
     }
     
-    public IActionResult OnGet(long id)
+    public IActionResult OnGet(long id, string? checkIn, string? checkOut)
     {
         Hotel = _hotelService.GetHotelById(id);
+        
         if (Hotel == null)
         {
             return NotFound();
         }
 
+        if (checkIn != null)
+        {
+            var decodedCheckIn = System.Web.HttpUtility.UrlDecode(checkIn);
+            CheckIn = DateTime.Parse(decodedCheckIn);
+        }
+
+        if (checkOut != null)
+        {
+            var decodedCheckOut = System.Web.HttpUtility.UrlDecode(checkOut);
+            CheckOut = DateTime.Parse(decodedCheckOut);
+        }
+
+        Hotel.Rooms = _hotelService.GetAvailableRoomsInHotel(id.ToString(), CheckIn, CheckOut);
+        
         var latitude = Hotel.Address.Latitude;
         var longitude = Hotel.Address.Longitude;
         var mapBbox = $"{(longitude - 0.005).ToString(CultureInfo.InvariantCulture)},{(latitude - 0.005).ToString(CultureInfo.InvariantCulture)},{(longitude + 0.005).ToString(CultureInfo.InvariantCulture)},{(latitude + 0.005).ToString(CultureInfo.InvariantCulture)}";
