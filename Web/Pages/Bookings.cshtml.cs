f8647a759a590c377e8569fe1212e2bb3961da70
ericomcolen <erico.colen@gmail.com>
2024-06-26 16:52:57 -0300

    Post de agendamentos

--- Web/Pages/Bookings.cshtml.cs
+++ Web/Pages/Bookings.cshtml.cs
@@ -1,6 +1,7 @@
 using Application.Services;
 using Domain;
 using Domain.Entities;
+using DTOs;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -26,6 +27,25 @@ public class BookingsModel : PageModel
             return NotFound();
         }
 
+        var currentUser = await _userManager.GetUserAsync(HttpContext.User);
+        Bookings = _bookingService.FindBookingsByUser(currentUser!.Id);
+
+        return Page();
+    }
+    
+    public async Task<IActionResult> OnPost(long roomId, DateTime checkIn, DateTime checkOut)
+    {
+        var user = await _userManager.GetUserAsync(HttpContext.User);
+        var dto = new CreateBookingDTO
+        {
+            UserId = user!.Id,
+            CheckIn = checkIn,
+            CheckOut = checkOut,
+            RoomId = roomId.ToString()
+        };
+        
+        _bookingService.Book(dto);
+        
         var currentUser = await _userManager.GetUserAsync(HttpContext.User);
         Bookings = _bookingService.FindBookingsByUser(currentUser!.Id);
 
