dbd2098df9acc5a5c8b8623f5b9d9fa3e45873ca
LucasCarvalho01 <lucascarvalhopc@gmail.com>
2024-07-07 17:02:19 -0300

    configurado libs de testes

---
+++ ServiceTests/HotelServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+using Moq;
+using Application.Services.Implementations;
+using Application.Repositories;
+using Domain.Entities;
+
+namespace ServiceTests;
+
+public class HotelServiceTest
+{
+    [Fact]
+    public void GetHotelById_ShouldReturnCorrectHotel()
+    {
+        // Arrange
+        var hotelId = "1";
+        var mockRepository = new Mock<HotelRepository>();
+        var hotelService = new HotelServiceImp(mockRepository.Object);
+        
+        mockRepository.Setup(repo => repo.GetById(It.IsAny<long>()))
+            .Returns((long id) => new Hotel { Id = id, Name = "Hotel Test" });
+
+        // Act
+        var result = hotelService.GetHotelById(hotelId);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("Hotel Test", result.Name);
+    }
+}
