756155aabe7d24a7e2a3935a26c3b53b9b34612b
Andre <75341645+andrembarreto@users.noreply.github.com>
2024-07-08 22:23:46 -0300

    Adiciona alguns testes para método de verificação de disponibilidade
    Esse método é bem importante para o sistema e tem algumas restrições interessantes para testar.

--- ServiceTests/BookingServiceTest.cs
+++ ServiceTests/BookingServiceTest.cs
@@ -28,9 +28,117 @@ public class BookingServiceTest
         var bookingService = new BookingServiceImp(bookingRepositoryMock.Object, roomServiceMock.Object,
                                                    userServiceMock.Object, this.userManagerMock.Object);
         long roomId = 1;
-        var room = new Room { Id = roomId };
         bookingRepositoryMock.Setup(repo => repo.GetBookingsByRoomId(roomId)).Returns(new List<Booking>());
 
         var result = bookingService.CheckRoomAvailabilityWithinPeriod(roomId, DateTime.Now, DateTime.Now);
+    }
+
+    [Fact]
+    public void CheckRoomAvailabilityWithinPeriod_ShouldReturnFalse_IfRoomHasCheckInWithinPeriod()
+    {
+        var bookingRepositoryMock = new Mock<BookingRepository>();
+        var roomServiceMock = new Mock<RoomService>();
+        var userServiceMock = new Mock<AppUserService>();
+
+        var bookingService = new BookingServiceImp(bookingRepositoryMock.Object, roomServiceMock.Object,
+                                                   userServiceMock.Object, this.userManagerMock.Object);
+
+        long roomId = 1;
+        var booking = new Booking
+        {
+            CheckIn = new DateTime(2000, 01, 02),
+            CheckOut = new DateTime(2000, 01, 04),
+            Room = new Room { Id = roomId },
+            User = new IdentityUser()
+        };
+        bookingRepositoryMock.Setup(repo => repo.GetBookingsByRoomId(roomId)).Returns(new List<Booking>() { booking });
+
+        var result = bookingService.CheckRoomAvailabilityWithinPeriod(roomId, new DateTime(2000, 01, 01), new DateTime(2000, 01, 03));
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void CheckRoomAvailabilityWithinPeriod_ShouldReturnFalse_IfRoomHasCheckOutWithinPeriod()
+    {
+        var bookingRepositoryMock = new Mock<BookingRepository>();
+        var roomServiceMock = new Mock<RoomService>();
+        var userServiceMock = new Mock<AppUserService>();
+
+        var bookingService = new BookingServiceImp(bookingRepositoryMock.Object, roomServiceMock.Object,
+                                                   userServiceMock.Object, this.userManagerMock.Object);
+
+        long roomId = 1;
+        var booking = new Booking
+        {
+            CheckIn = new DateTime(2000, 01, 01),
+            CheckOut = new DateTime(2000, 01, 03),
+            Room = new Room { Id = roomId },
+            User = new IdentityUser()
+        };
+        bookingRepositoryMock.Setup(repo => repo.GetBookingsByRoomId(roomId)).Returns(new List<Booking>() { booking });
+
+        var result = bookingService.CheckRoomAvailabilityWithinPeriod(roomId, new DateTime(2000, 01, 02), new DateTime(2000, 01, 04));
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void CheckRoomAvailabilityWithinPeriod_ShouldReturnTrue_IfRoomHasBookingWithCheckInOnQueryCheckOut()
+    {
+        var bookingRepositoryMock = new Mock<BookingRepository>();
+        var roomServiceMock = new Mock<RoomService>();
+        var userServiceMock = new Mock<AppUserService>();
+
+        var bookingService = new BookingServiceImp(bookingRepositoryMock.Object, roomServiceMock.Object,
+                                                   userServiceMock.Object, this.userManagerMock.Object);
+
+        long roomId = 1;
+        var booking = new Booking
+        {
+            CheckIn = new DateTime(2000, 01, 02),
+            CheckOut = new DateTime(2000, 01, 03),
+            Room = new Room { Id = roomId },
+            User = new IdentityUser()
+        };
+        bookingRepositoryMock.Setup(repo => repo.GetBookingsByRoomId(roomId)).Returns(new List<Booking>() { booking });
+
+        var result = bookingService.CheckRoomAvailabilityWithinPeriod(roomId, new DateTime(2000, 01, 01), new DateTime(2000, 01, 02));
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void CheckRoomAvailabilityWithinPeriod_ShouldReturnTrue_IfRoomHasBookingWithCheckOutOnQueryCheckIn()
+    {
+        var bookingRepositoryMock = new Mock<BookingRepository>();
+        var roomServiceMock = new Mock<RoomService>();
+        var userServiceMock = new Mock<AppUserService>();
+
+        var bookingService = new BookingServiceImp(bookingRepositoryMock.Object, roomServiceMock.Object,
+                                                   userServiceMock.Object, this.userManagerMock.Object);
+
+        long roomId = 1;
+        var booking = new Booking
+        {
+            CheckIn = new DateTime(2000, 01, 01),
+            CheckOut = new DateTime(2000, 01, 02),
+            Room = new Room { Id = roomId },
+            User = new IdentityUser()
+        };
+        bookingRepositoryMock.Setup(repo => repo.GetBookingsByRoomId(roomId)).Returns(new List<Booking>() { booking });
+
+        var result = bookingService.CheckRoomAvailabilityWithinPeriod(roomId, new DateTime(2000, 01, 02), new DateTime(2000, 01, 03));
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void CheckRoomAvailabilityWithinPeriod_ShouldThrowException_IfCheckInIsNotBeforeCheckOut()
+    {
+        var bookingRepositoryMock = new Mock<BookingRepository>();
+        var roomServiceMock = new Mock<RoomService>();
+        var userServiceMock = new Mock<AppUserService>();
+
+        var bookingService = new BookingServiceImp(bookingRepositoryMock.Object, roomServiceMock.Object,
+                                                   userServiceMock.Object, this.userManagerMock.Object);
+
+        Assert.Throws<ArgumentException>(() => bookingService.CheckRoomAvailabilityWithinPeriod(1, new DateTime(2000, 01, 02), new DateTime(2000, 01, 01)));
     }
 }
